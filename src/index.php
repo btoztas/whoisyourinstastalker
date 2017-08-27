e107767346db135698ad6e5aa500dc2affc6607d
Bruno Goncalves <btoztas@gmail.com>
2017-08-27 11:08:48 +0100

    added a note to index

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
   <center>
     <h1>Who is your biggest latest Instagram stalker?</h1>
     <h2>Check who most liked your last 20 photos<h2>
-    <h3>Your profile must be public!<h3>
+    <h3>Your profile must be public! It should give you the result in ~20 seconds. If it doesn't, try again.<h3>
     <form action='get_statistics.php'>
       <h3>Instagram User:</h3>
       <input type="text" name="user" />
